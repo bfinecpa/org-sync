0612db02f723ab64e3736f774d1922184881de3c
한주덕 <bfinecpa@daou.co.kr>
2025-12-25 16:03:12 +0900

    feat : 리팩토링

--- org-sync-spring/src/main/java/org/orgsync/spring/lock/JdbcLockManager.java
+++ org-sync-spring/src/main/java/org/orgsync/spring/lock/JdbcLockManager.java
@@ -1,5 +1,6 @@
 package org.orgsync.spring.lock;
 
+import java.sql.ResultSet;
 import org.orgsync.core.lock.LockManager;
 
 import org.springframework.jdbc.core.ResultSetExtractor;
@@ -40,8 +41,8 @@ public class JdbcLockManager implements LockManager {
     @Override
     public void withLock(String companyUuid, Runnable runnable) {
         transactionTemplate.executeWithoutResult(status -> {
-            boolean locked = jdbcTemplate.query(lockSql, Map.of("companyUuid", companyUuid),
-                    (ResultSetExtractor<Boolean>) (rs) -> rs.next());
+            boolean locked = Boolean.TRUE.equals(
+                jdbcTemplate.query(lockSql, Map.of("companyUuid", companyUuid), ResultSet::next));
 
             if (!locked) {
                 throw new IllegalStateException("No company row found for uuid=" + companyUuid);
