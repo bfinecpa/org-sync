00e2886e3504db31e536a2996e96364d9e9850de
Jooduk Han <79684851+bfinecpa@users.noreply.github.com>
2026-01-05 21:07:35 +0900

    Handle syncing for additional domains

--- org-sync-core/src/main/java/org/orgsync/core/dto/IntegrationDto.java
+++ org-sync-core/src/main/java/org/orgsync/core/dto/IntegrationDto.java
@@ -1,5 +1,23 @@
 package org.orgsync.core.dto;
 
-public class IntegrationDto {
+public class IntegrationDto implements Settable {
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void set(LogInfoDto logInfoDto) {
+        if (logInfoDto == null || logInfoDto.updatedValue() == null) {
+            return;
+        }
+        if ("id".equals(logInfoDto.fieldName())) {
+            setId(Long.valueOf(logInfoDto.updatedValue().toString()));
+        }
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
 }
